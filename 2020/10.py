4f76614de39cce019d8d7e2864142e65346b00a9
SooOverpowered <lehoangminh15122000@gmail.com>
2020-12-11 08:13:57 +0700

    commit

--- 2020/10.py
+++ 2020/10.py
@@ -1,5 +1,24 @@
 def part1(data):
-    pass
+    lines = [int(num) for num in data.rstrip().split('\n')]
+    lines = sorted(lines)
+    lines.append(lines[-1]+3)
+    lines.insert(0, 0)
+    count = {'1': 0, '3': 0}
+    for i in range(len(lines)-1):
+        if lines[i]+1 == lines[i+1]:
+            count['1'] += 1
+        elif lines[i]+3 == lines[i+1]:
+            count['3'] += 1
+    return count['3']*count['1']
+
 
 def part2(data):
-    pass
+    lines = [int(num) for num in data.rstrip().split('\n')]
+    lines = sorted(lines)
+    search_dict={0:1}
+    for adapter in lines:
+        search_dict[adapter]=0
+        for i in range(1,4):
+            if adapter-i==0 or adapter-i in lines:
+                search_dict[adapter]+=search_dict[adapter-i]
+    return search_dict[lines[-1]]
