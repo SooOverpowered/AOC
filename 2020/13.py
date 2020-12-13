0ef1ca6ac9cef9ce20ec4de6c7f012e8e65bea39
SooOverpowered <lehoangminh15122000@gmail.com>
2020-12-13 17:26:05 +0700

    commit

--- 2020/13.py
+++ 2020/13.py
@@ -1,5 +1,29 @@
 def part1(data):
-    pass
+    lines=data.rstrip().split('\n')
+    lines=[int(lines[0]), [int(busid) for busid in lines[1].split(',') if busid != 'x']]
+    nearest=None
+    current=0
+    for busid in lines[1]:
+        if nearest==None:
+            nearest=busid-lines[0]%busid
+            current=busid
+        elif lines[0]%busid==0:
+            return 0
+        else:
+            if nearest>busid-lines[0]%busid:
+                nearest=busid-lines[0]%busid
+                current=busid
+    return nearest*current
 
 def part2(data):
-    pass
+    lines= data.rstrip().split('\n')[1].split(',')
+    lines=[(i,int(lines[i])) for i in range(len(lines)) if lines[i]!='x']
+    jump=lines[0][1]
+    time_stamp=0
+    for item in lines[1:]:
+        while (time_stamp+item[0])%item[1] != 0:
+            time_stamp+=jump
+        jump*=item[1]
+    return time_stamp
+
+print(part2(open('13.txt','r').read()))
